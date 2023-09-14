ca782d4611501f6fb329a59b6841349915c3bc08
Theborna <borna710@gmail.com>
2023-09-14 18:19:26 +0330

    updated hero text and profile images

--- script/script_3.js
+++ script/script_3.js
@@ -12,3 +12,45 @@ for (i = 0; i < coll.length; i++) {
         }
     });
 }
+
+const typedText = document.querySelector('.typed-text');
+const itemsAttribute = typedText.getAttribute('data-typed-items');
+const textItems = itemsAttribute.split(', ').map(item => item.trim());
+let index = 0;
+let currentText = '';
+let isErasing = false;
+
+function type() {
+    if (!isErasing) {
+        currentText = textItems[index].substring(0, currentText.length + 1);
+        typedText.textContent = currentText;
+    }
+
+    if (currentText === textItems[index] && !isErasing) {
+        isErasing = true;
+        setTimeout(erase, 1000);
+    } else if (currentText === '' && isErasing) {
+        isErasing = false;
+        index = (index + 1) % textItems.length;
+        setTimeout(type, 500);
+    } else {
+        setTimeout(type, 100);
+    }
+}
+
+function erase() {
+    if (isErasing) {
+        currentText = textItems[index].substring(0, currentText.length - 1);
+        typedText.textContent = currentText;
+    }
+
+    if (currentText === '') {
+        isErasing = false;
+        index = (index + 1) % textItems.length;
+        setTimeout(type, 500);
+    } else {
+        setTimeout(erase, 100);
+    }
+}
+
+type();
